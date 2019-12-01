84002116501e43bc76088fc6e7c8a5e92f18e5ce
15919873635 <15919873635@126.com>
2019-12-01 11:32:29 +0800

    配置起windows下的开发环境，添加vscode配置

---
+++ compiler/index.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    printf("This is a test file!\r\nInput a number: ");
+    scanf("%d",&n);
+    printf("The number is %d",n);
+    return 0;
+}
